1c861c21be9d27fa10e26ef54accb9bf794c7430
HIEU HUYNH <hieuhuynhif@gmail.com>
2022-11-24 18:50:17 +0700

    24-11-2022

--- MVC_Shopping/Core/mvc_shopping.sql
+++ MVC_Shopping/Core/mvc_shopping.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 23, 2022 at 01:21 PM
--- Server version: 10.4.25-MariaDB
--- PHP Version: 8.1.10
+-- Generation Time: Nov 24, 2022 at 12:49 PM
+-- Server version: 10.4.27-MariaDB
+-- PHP Version: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,7 +30,7 @@ SET time_zone = "+00:00";
 CREATE TABLE `color` (
   `id` int(11) NOT NULL,
   `color` varchar(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `color`
@@ -48,24 +48,42 @@ INSERT INTO `color` (`id`, `color`) VALUES
 --
 
 CREATE TABLE `detail_order` (
-  `id` int(11) NOT NULL,
-  `id_order` int(11) NOT NULL,
-  `id_product` int(11) NOT NULL,
-  `id_color` int(11) NOT NULL,
-  `id_size` int(11) NOT NULL,
-  `qty` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `id` int(255) NOT NULL,
+  `id_order` int(255) NOT NULL,
+  `id_product` int(255) NOT NULL,
+  `color` varchar(15) NOT NULL,
+  `size` int(30) NOT NULL,
+  `qty` int(30) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `detail_order`
+--
+
+INSERT INTO `detail_order` (`id`, `id_order`, `id_product`, `color`, `size`, `qty`) VALUES
+(1, 0, 21, 'red', 7, 2),
+(2, 0, 21, 'red', 9, 2),
+(3, 0, 21, 'black', 6, 2);
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `order`
+-- Table structure for table `orders`
 --
 
-CREATE TABLE `order` (
+CREATE TABLE `orders` (
   `id` int(11) NOT NULL,
-  `username` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `username` varchar(255) NOT NULL,
+  `sdt` varchar(15) NOT NULL,
+  `time` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `orders`
+--
+
+INSERT INTO `orders` (`id`, `username`, `sdt`, `time`) VALUES
+(1, 'test', '111', '11/24/2022, 2:54:28 PM');
 
 -- --------------------------------------------------------
 
@@ -82,7 +100,7 @@ CREATE TABLE `products` (
   `src` varchar(255) NOT NULL,
   `detail` text NOT NULL,
   `status` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `products`
@@ -103,7 +121,7 @@ CREATE TABLE `quantity` (
   `id_color` int(11) NOT NULL,
   `id_size` int(255) NOT NULL,
   `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -114,7 +132,7 @@ CREATE TABLE `quantity` (
 CREATE TABLE `size` (
   `id` int(11) NOT NULL,
   `name` int(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 -- --------------------------------------------------------
 
@@ -130,14 +148,15 @@ CREATE TABLE `user` (
   `src` varchar(255) NOT NULL,
   `likes` varchar(255) NOT NULL,
   `role` varchar(10) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `user`
 --
 
 INSERT INTO `user` (`id`, `username`, `password`, `sdt`, `src`, `likes`, `role`) VALUES
-(68, 'admin', 'admin', '999999999', '', '', 'admin');
+(68, 'admin', 'admin', '999999999', '', '', 'admin'),
+(70, 'test', '111', '111', '', '', 'customer');
 
 --
 -- Indexes for dumped tables
@@ -156,9 +175,9 @@ ALTER TABLE `detail_order`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `order`
+-- Indexes for table `orders`
 --
-ALTER TABLE `order`
+ALTER TABLE `orders`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -201,13 +220,13 @@ ALTER TABLE `color`
 -- AUTO_INCREMENT for table `detail_order`
 --
 ALTER TABLE `detail_order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `order`
+-- AUTO_INCREMENT for table `orders`
 --
-ALTER TABLE `order`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+ALTER TABLE `orders`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -231,7 +250,7 @@ ALTER TABLE `size`
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=70;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=71;
 
 --
 -- Constraints for dumped tables
